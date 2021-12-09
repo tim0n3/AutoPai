69a16474dca71039ee130fa78229840a6a50e8a8
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2021-12-09 16:24:34 +0200

    Update 3-post-install.sh

--- 3-post-install.sh
+++ 3-post-install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-
-echo -e "remember to paste your rsa cert into IoT Core\n"
-
-cat rsa_cert.pem
-
-echo -e "and remember to update the parameters of the config.json and iot-core-config.json files respectively!!!\n"
-
-
-
-echo -e "\nDone!\n"
-
-echo "Reboot the Pi now"
+echo -e "remember to paste your rsa cert into IoT Core\n" ;
+cat rsa_cert.pem ;
+echo -e "and remember to update the parameters of the config.json and iot-core-config.json files respectively!!!\n" ;
+echo -e "\nDone!\n" ;
+echo "Reboot the Pi now" ;
