af704e6dd3f64fee8138a7ab377437c57d42b9aa
tim0n3 <>
2021-12-13 15:13:39 +0200

    corrected countdown before shutdown

--- 3-post-install.sh
+++ 3-post-install.sh
@@ -5,12 +5,12 @@ echo -e "and remember to update the parameters of the config.json and iot-core-c
 echo -e "\nDone!\n"
 echo "Reboot the Pi now"
 echo "Rebooting the Pi now"
-echo "going down in 30..." && wait 10
-echo "going down in 20..." && wait 10
-echo "going down in 10..." && wait 5
-echo "going down in 5..." && wait 1
-echo "going down in 4..." && wait 1
-echo "going down in 3..." && wait 1
-echo "going down in 2..." && wait 1
-echo "bye bye" && wait 1
+echo "going down in 30..." && sleep 10
+echo "going down in 20..." && sleep 10
+echo "going down in 10..." && sleep 5
+echo "going down in 5..." && sleep 1
+echo "going down in 4..." && sleep 1
+echo "going down in 3..." && sleep 1
+echo "going down in 2..." && sleep 1
+echo "bye bye" && sleep 1
 sudo reboot now
