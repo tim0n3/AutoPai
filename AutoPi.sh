ddef8d24f0d31875d34720f38d5a9e8fcec14c14
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2021-12-13 13:30:55 +0200

    Update AutoPi.sh

--- AutoPi.sh
+++ AutoPi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo bash /home/pi/AutoPai/0-preinstall.sh
-bash /home/pi/AutoPai/1-setup.sh
-sudo runuser -u pi bash /home/pi/AutoPai/2-user.sh
-sudo runuser -u pi bash /home/pi/AutoPai/3-post-install.sh
+sudo bash ./0-preinstall.sh
+bash ./1-setup.sh
+sudo runuser -u pi bash ~/AutoPai/2-user.sh
+sudo runuser -u pi bash ~/AutoPai/3-post-install.sh
 echo "--------------------------------------"
 echo "-- AutoPi has completed autoconfigs --"
 echo "--------------------------------------"
