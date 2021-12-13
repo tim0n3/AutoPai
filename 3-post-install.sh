7e4bbb4aebcb5de49dda74cc73d98887656df94c
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2021-12-13 11:07:13 +0200

    Update 3-post-install.sh
    removed the ";"

--- 3-post-install.sh
+++ 3-post-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo -e "remember to paste your rsa cert into IoT Core\n" ;
-cat rsa_cert.pem ;
-echo -e "and remember to update the parameters of the config.json and iot-core-config.json files respectively!!!\n" ;
-echo -e "\nDone!\n" ;
-echo "Reboot the Pi now" ;
-echo "Rebooting the Pi now" ;
-echo "going down in 30..." && wait 10 ;
-echo "going down in 20..." && wait 10 ;
-echo "going down in 10..." && wait 5 ;
-echo "going down in 5..." && wait 1 ;
-echo "going down in 4..." && wait 1 ;
-echo "going down in 3..." && wait 1 ;
-echo "going down in 2..." && wait 1 ;
-echo "bye bye" && wait 1 ;
-sudo reboot now ;
+echo -e "remember to paste your rsa cert into IoT Core\n"
+cat rsa_cert.pem
+echo -e "and remember to update the parameters of the config.json and iot-core-config.json files respectively!!!\n"
+echo -e "\nDone!\n"
+echo "Reboot the Pi now"
+echo "Rebooting the Pi now"
+echo "going down in 30..." && wait 10
+echo "going down in 20..." && wait 10
+echo "going down in 10..." && wait 5
+echo "going down in 5..." && wait 1
+echo "going down in 4..." && wait 1
+echo "going down in 3..." && wait 1
+echo "going down in 2..." && wait 1
+echo "bye bye" && wait 1
+sudo reboot now
