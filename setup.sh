3404682283c7124081fb0bc8265948f084ed3ebf
tim0n3 <>
2021-12-15 15:40:32 +0200

    formatted ifstatement similarly to other scripts

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@ echo "Installing git to clone the AutoPai repo"
 sudo apt install git -y
 echo "Check if using Pi or moxa:"
 read -p "Is this a Pi or a Moxa? (y/n) :" ispi
-if [[ $ispi -eq y ]]
-then
-echo " Device is Raspberry Pi "
-echo " Using Pi-scripts "
-git clone https://github.com/tim0n3/AutoPai.git ;\
-cd AutoPai ;\
-chmod +x *.sh ;\
-bash AutoPi.sh
+if [[ $ispi -eq y ]];
+	then
+		echo " Device is Raspberry Pi "
+		echo " Using Pi-scripts "
+		git clone https://github.com/tim0n3/AutoPai.git ;\
+		cd AutoPai ;\
+		chmod +x *.sh ;\
+		bash AutoPi.sh
 else
-echo " Device is Moxa "
-echo "Using Moxa-scripts "
-bash AutoMoxa.sh
+	echo " Device is Moxa "
+	echo "Using Moxa-scripts "
+	bash AutoMoxa.sh
 fi
