14d9d0c46f85219e1580c32b9acccc8ddfbed451
tim0n3 <>
2021-12-17 12:27:06 +0200

    experimenting with functions and if..else nested within a function.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-echo "Installing git to clone the AutoPai repo"
-sudo apt install git -y
-echo "Check if using Pi or moxa:"
-read -n1 -p "Is this a Pi or a Moxa? (y/n) :" ispi
-if [[ $ispi -eq "y" || $ispi -eq "Y" ]]; then
-	echo " Device is Raspberry Pi "
-	echo " Using Pi-scripts "
-	git clone https://github.com/tim0n3/AutoPai.git ;\
-	cd AutoPai ;\
-	chmod +x *.sh ;\
+
+function _dependancy_install() {
+	echo "Installing git to clone the AutoPai repo"
+	sudo apt install git -y
+}
+
+function _is_pi() {
+	git clone https://github.com/tim0n3/AutoPai.git ;
+	cd AutoPai ;
+	chmod +x *.sh ;
 	bash AutoPi.sh
-else
-	echo " Device is Moxa "
-	echo "Using Moxa-scripts "
-	git clone https://github.com/tim0n3/AutoPai.git ;\
-	cd AutoPai ;\
-	chmod +x *.sh ;\
+}
+
+function _is_moxa() {
+	git clone https://github.com/tim0n3/AutoPai.git ;
+	cd AutoPai ;
+	chmod +x *.sh ;
 	bash AutoMoxa.sh
-fi
+}
+
+function _start() {
+	echo "Check if using Pi or moxa:"
+	read -n1 -p "Is this a Pi or a Moxa? (y/n) :" ispi
+	if [[ $ispi -eq "y" || $ispi -eq "Y" ]]; then
+		echo " Device is Raspberry Pi "
+		echo " Using Pi-scripts "
+		_is_pi
+	else
+		echo " Device is Moxa "
+		echo "Using Moxa-scripts "
+		_is_moxa
+	fi
+}
+
+_dependancy_install
+_start
