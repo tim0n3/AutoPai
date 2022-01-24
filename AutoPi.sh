6c9f4cff7e953d9aba620073dca1ea984a845fac
tim0n3 <>
2022-01-24 10:41:10 +0200

    corrected non-sudo invocation of 1-setup.sh

--- AutoPi.sh
+++ AutoPi.sh
@@ -2,7 +2,7 @@
 echo "Set x permission for .sh files in this folder"
 chmod +x *.sh
 sudo bash ./0-preinstall.sh
-bash ./1-setup.sh
+sudo bash ./1-setup.sh
 sudo runuser -u pi bash ~/AutoPai/2-user.sh
 sudo runuser -u pi bash ~/AutoPai/3-post-install.sh
 echo "--------------------------------------"
