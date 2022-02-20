047ccf723b839e31687afe2aa97eb17030e91ebc
tim0n3 <>
2022-02-20 14:54:05 +0200

    added distro checks to ensure you're running the script on a supported distro.
    added pip requirements file and pip command to install them after apt packages installation

--- setup.sh
+++ setup.sh
@@ -11,16 +11,28 @@ _dependancy_install() {
 
 _is_pi() {
 	echo "running AutoPi.sh and linked scripts"
-	bash AutoPi.sh
+	bash 1.10-rc1-ubuntu.sh
 }
 
 _is_moxa() {
-	echo "running AutoMoxa.sh and linked scripts"
-	bash AutoMoxa.sh
+	echo "Moxa's aren't support due to python wheels compilation requirements."
+}
+function _os_check() {
+	echo "Checking if using the correct distro:"
+	distribution=$(egrep '^(NAME)=' /etc/os-release)
+    codename=$( egrep '^(VERSION)=' /etc/os-release)
+    if [[ ! $distribution =~ ^(Debian|Ubuntu)$ ]]; then
+        echo_error "Your distribution ($distribution) is not supported. Swizzin requires Ubuntu or Debian."
+        exit 1
+    fi
+    if [[ ! $codename =~ ^(buster|Focal|bullseye|Jammy)$ ]]; then
+        echo_error "Your release ($codename) of $distribution is not supported."
+        exit 1
+    fi
 }
 
 function _start() {
-	echo "Check if using Pi or moxa:"
+	_os_check
 	read -n1 -p "Is this a Pi or a Moxa? (y=Pi/n=moxa) (y/n) :" ispi
 	case ${ispi:0:1} in
 		y|Y )
