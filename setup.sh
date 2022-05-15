248f0ce5e0d4ea56671cccf7818320ac281198c9
tim0n3 <>
2022-05-15 08:17:30 +0200

    added function to check distro as this branch is only meant for raspberry pi OS 64 bit - and wrapped everything in the setup file into the _main function

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # This script must be run with sudo
-touch /home/pi/AutoPai/stout-setup.log ;\
-time sudo bash /home/pi/AutoPai/1.10-rc1-raspOS.sh  >> stout-setup.log 2>&1 
+function _main() {
+	function _os_check() {
+	    echo "Checking if using the correct distro:"
+	    # Borrowing this check from https://github.com/swizzin/swizzin
+	    distribution=$(lsb_release -is)
+        codename=$(lsb_release -cs)
+        if [[ ! $distribution =~ ^(Debian)$ ]]; then
+            echo "Your distribution ($distribution) is not supported. This script requires Debian."
+            exit 1
+        fi
+        if [[ ! $codename =~ ^(buster|bullseye)$ ]]; then
+            echo "Your release ($codename) of $distribution is not supported."
+            exit 1
+        fi
+    }
+
+    function _run_setup() {
+        touch /home/pi/AutoPai/stout-setup.log ;\
+        time sudo bash /home/pi/AutoPai/1.10-rc1-raspOS.sh  >> stout-setup.log 2>&1 
+    }
+_os_check;
+_run_setup;
+}
+_main;
